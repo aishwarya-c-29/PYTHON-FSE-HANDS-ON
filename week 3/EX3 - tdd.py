837c2b40268c2540e234d1cd0c8daa7756ad81af
aishwarya-c-29 <kalaiselviaishwarya2910@gmail.com>
2026-07-17 19:18:31 +0530

    Create EX3 - tdd.py

---
+++ week 3/EX3 - tdd.py
@@ -0,0 +1,56 @@
+import unittest
+
+# Calculator Class
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+
+# Unit Tests
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(10, 5), 15)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 5), 2)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(10, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+Output:
+
+.....
+----------------------------------------------------------------------
+Ran 5 tests in 0.001s
+
+OK
+Red : Yes
+Green : Yes
+Refactor : Yes
